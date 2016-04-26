8c5b43e004863e825c933f0905ea9f5de27fbe06
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-04-26 10:29:29 -0600

    setting up search controller

---
+++ spec/feature/get_products_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "hit best buy api and return product information" do
+  it "user sees 15 results" do
+    visit root_path
+
+    fill_in 'Search', with: 'sennheiser'
+    click_on "Search"
+
+    expect(current_path).to eq '/search'
+
+    expect(page).to have_content "sku"
+    expect(page).to have_content "name"
+    expect(page).to have_content "customer average review"
+    expect(page).to have_content "short description"
+    expect(page).to have_content "sale price"
+    expect(page).to have_content "image"
+  end
+end
+# As a user
+# When I visit the "/"
+# And I fill in the search box with "sennheiser" and click "search"
+# Then my current path should be "/search"
+# And I should see exactly 15 results
+# And each result should contain sku, name, customer average review, short description, sale price, and image ONLY
